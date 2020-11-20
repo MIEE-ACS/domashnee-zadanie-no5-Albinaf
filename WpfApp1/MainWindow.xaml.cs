207cae343a3cd381a908b7cf1b828f1ed0285774
Albinaf <71094247+Albinaf@users.noreply.github.com>
2020-11-20 19:12:28 +1100

    Исправленное задание

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Printing;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,20 +23,27 @@ namespace WpfApp1
             Паста,
             Помада,
             Крем,
-            Пудра
+            Пудра,
+            Any
         }
 
-        private Name m_name;
-        private int m_vendorCode;
-        private decimal m_price;
-        public DateTime m_date;
-        private int m_expirationMonths;
+        public Name name;
+        public int vendorCode;
+        public decimal price;
+        public DateTime date;
+        public int expirationMonths;
+
+        public Tovar(DateTime m_productionDate, int m_expirationMonths)
+        {
+            date = m_productionDate;
+            expirationMonths = m_expirationMonths;
+        }
 
         public DateTime Date
         {
             get
             {
-                return m_date;
+                return date;
             }
         }
 
@@ -43,7 +51,7 @@ namespace WpfApp1
         {
             get
             {
-                return ((DateTime.Now - m_date).TotalDays / 30) >= m_expirationMonths;
+                return ((DateTime.Now - date).TotalDays / 30) >= expirationMonths;
             }
         }
 
@@ -52,12 +60,12 @@ namespace WpfApp1
             set
             {
                 if (value > 0)
-                    m_vendorCode = value;
+                    vendorCode = value;
             }
 
             get
             {
-                return m_vendorCode;
+                return vendorCode;
             }
         }
 
@@ -66,12 +74,12 @@ namespace WpfApp1
             set
             {
                 if (value > 0)
-                    m_expirationMonths = value;
+                    expirationMonths = value;
             }
 
             get
             {
-                return m_expirationMonths;
+                return expirationMonths;
             }
         }
 
@@ -80,111 +88,142 @@ namespace WpfApp1
             set
             {
                 if (value > 0)
-                    m_price = value;
+                    price = value;
             }
             get
             {
-                return m_price;
+                return price;
             }
         }
 
-        public Tovar(Name name, int vendorCode, decimal price, DateTime productionDate, int expirationMonths)
+        public override string ToString()
         {
-            m_name = name;
-            m_vendorCode = vendorCode;
-            m_price = price;
-            m_date = productionDate;
-            m_expirationMonths = expirationMonths;
+            string name_name;
+
+            switch (name)
+            {
+                case Name.Паста:
+                    name_name = "Зубная паста";
+                    break;
+                case Name.Крем:
+                    name_name = "Крем";
+                    break;
+                case Name.Помада:
+                    name_name = "Помада";
+                    break;
+                case Name.Пудра:
+                    name_name = "Пудра";
+                    break;
+                default:
+                    name_name = "";
+                    break;
+            }
+            return $"{name_name}, {vendorCode}, {price}, {date}, {expirationMonths}";
         }
     }
 
     public partial class MainWindow : Window
     {
+        List<Tovar> tovars = new List<Tovar>
+        {
+            new Tovar (new DateTime(2020,12,23), 6) { name = Tovar.Name.Крем, vendorCode = 182, price = 254},
+            new Tovar (new DateTime(2020,11,20), 3) { name = Tovar.Name.Пудра, vendorCode = 456, price = 145},
+            new Tovar (new DateTime(2020,9,14), 2) { name = Tovar.Name.Паста, vendorCode = 123, price = 123},
+            new Tovar (new DateTime(2020,2,21), 9) { name = Tovar.Name.Помада, vendorCode = 442, price = 200},
+        };
+
+        public void updateTovarList()
+        {
+            lbTovar.Items.Clear();
+
+            var selectedType = (Tovar.Name)cbNameFilter.SelectedIndex;
+           
+            foreach (var tovar in tovars)
+            {
+                if (tovar.name == selectedType || selectedType == Tovar.Name.Any)
+                    lbTovar.Items.Add(tovar);
+            }
+        }
         public MainWindow()
         {
             InitializeComponent();
+            updateTovarList();
+        }
+
+        private void cbTypeFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            updateTovarList();
         }
 
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                int vendorCode = int.Parse(tbVC.Text);
-                decimal price = decimal.Parse(tbP.Text);
-                int day = int.Parse(tbD.Text);
-                int month = int.Parse(tbM.Text);
-                int year = int.Parse(tbY.Text);
-                int expirationMonths = int.Parse(tbSL.Text);
-                int nTovar = cbName.SelectedIndex;
-                DateTime date1 = new DateTime(year, month, day);
 
-                switch (nTovar)
+                foreach (var tvr in tovars)
                 {
-                    case 0:
-                        Tovar tovars = new Tovar(Tovar.Name.Паста, vendorCode, price, date1, expirationMonths);
-      
-                        int compare = DateTime.Compare(date1.AddMonths(expirationMonths), DateTime.Now);
+                    if (tvr.vendorCode == int.Parse(tbVC.Text))
+                    {
+                        MessageBox.Show("Артикулы не могут быть одинаковыми!", "Проверьте введенные значения", MessageBoxButton.OK, MessageBoxImage.Error);
+                        
+                        return;
+                    }
 
-                        if (compare < 0)
-                        {
-                            lbTovar.Items.Add($"{Tovar.Name.Паста}, Артикул: {vendorCode}, Цена: {price}, Дата выпуска: {date1}, Срок годности(мес.): {expirationMonths}, На сегоднящний день товар годен.");
-                        }
-                        else
-                        {
-                            lbTovar.Items.Add($"{Tovar.Name.Паста}, Артикул: {vendorCode}, Цена: {price}, Дата выпуска: {date1}, Срок годности(мес.): {expirationMonths}, На сегоднящний день товар годен.");
-                        }
-                        break;
-                    case 1:
-                        tovars = new Tovar(Tovar.Name.Помада, vendorCode, price, date1, expirationMonths);
-
-                        compare = DateTime.Compare(date1.AddMonths(expirationMonths), DateTime.Now);
-
-                        if (compare < 0)
-                        {
-                            lbTovar.Items.Add($"{Tovar.Name.Помада}, Артикул: {vendorCode}, Цена: {price}, Дата выпуска: {date1}, Срок годности(мес.): {expirationMonths}, На сегоднящний день товар годен.");
-                        }
-                        else
-                        {
-                            lbTovar.Items.Add($"{Tovar.Name.Помада}, Артикул: {vendorCode}, Цена: {price}, Дата выпуска: {date1}, Срок годности(мес.): {expirationMonths}, На сегоднящний день товар годен.");
-                        }
-                        break;
-                    case 2:
-                        tovars = new Tovar(Tovar.Name.Крем, vendorCode, price, date1, expirationMonths);
-
-                        compare = DateTime.Compare(date1.AddMonths(expirationMonths), DateTime.Now);
-
-                        if (compare < 0)
-                        {
-                            lbTovar.Items.Add($"{Tovar.Name.Крем}, Артикул: {vendorCode}, Цена: {price}, Дата выпуска: {date1}, Срок годности(мес.): {expirationMonths}, На сегоднящний день товар годен.");
-                        }
-                        else
-                        {
-                            lbTovar.Items.Add($"{Tovar.Name.Крем}, Артикул: {vendorCode}, Цена: {price}, Дата выпуска: {date1}, Срок годности(мес.): {expirationMonths}, На сегоднящний день товар годен.");
-                        }
-                        break;
-                    case 3:
-                        tovars = new Tovar(Tovar.Name.Пудра, vendorCode, price, date1, expirationMonths);
-
-                        compare = DateTime.Compare(date1.AddMonths(expirationMonths), DateTime.Now);
-
-                        if (compare < 0)
-                        {
-                            lbTovar.Items.Add($"{Tovar.Name.Пудра}, Артикул: {vendorCode}, Цена: {price}, Дата выпуска: {date1}, Срок годности(мес.): {expirationMonths}, На сегоднящний день товар годен.");
-                        }
-                        else
-                        {
-                            lbTovar.Items.Add($"{Tovar.Name.Пудра}, Артикул: {vendorCode}, Цена: {price}, Дата выпуска: {date1}, Срок годности(мес.): {expirationMonths}, На сегоднящний день товар годен.");
-                        }
-                        break;
                 }
+                   
+                    Tovar tovar = new Tovar(new DateTime(int.Parse(tbY.Text), int.Parse(tbM.Text), int.Parse(tbD.Text)), int.Parse(tbSL.Text))
+                    {
+                        vendorCode = int.Parse(tbVC.Text),
+                        name = (Tovar.Name)cbName.SelectedIndex,
+                        price = int.Parse(tbP.Text)
+                    };
 
+                    tovars.Add(tovar);
+
+                    updateTovarList();
+                
+                    
+                
             }
-            catch(Exception ex)
+            catch (FormatException)
+            {             
+                MessageBox.Show("Проверьте введенные значения", "Ошибка данных", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception ex)
             {
-                MessageBox.Show(ex.Message);
+                MessageBox.Show("Обратитесь к разработчику: " + ex.Message, "Неизвестная ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
 
+        }
 
+
+        private void btnCh_Click(object sender, RoutedEventArgs e)
+        {
+            Tovar tovarr = lbTovar.SelectedItem as Tovar;
+
+            MessageBox.Show($"Дата изготовления:{tovarr.Date}, истёк ли срок (f - не истёк, t - истёк): {tovarr.isExpired}", "Проверка на срок годности", MessageBoxButton.OK);
+        }
+
+
+
+
+
+        private void lbTovar_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        try
+        {
+        Tovar tovarr = lbTovar.SelectedItem as Tovar;
+                tbVC.Text = tovarr.vendorCode.ToString();
+                tbP.Text = tovarr.price.ToString();
+                tbSL.Text = tovarr.expirationMonths.ToString();
+                tbY.Text = tovarr.Date.ToString();
+                tbD.Text = " ";
+                tbM.Text = " ";
+            }
+        catch (Exception ex)
+        {
+        MessageBox.Show("Обратитесь к разработчику: " + ex.Message, "Неизвестная ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
         }
     }
 
